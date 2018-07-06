1854cc6d29d7922fec61cbad7dc8f6105e751bc5
Jean-Baptiste Vilain <jbvilain@gmail.com>
2018-07-06 09:34:36 +0100

    Updated config values

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import {Params} from "@angular/router";
 import {InjectionToken} from "@angular/core";
 
 export const INIT_PARAMS = new InjectionToken<Params>('initParams');
-export const DB_URL = "";
-export const DB_AUTH = "";
-export const CONFIG_URL = "";
-export const CONFIG_AUTH = "";
+export const DEFAULT_STATE = new InjectionToken<string[]>('defaultState');
+export const DB_URL = '';
+
+export const OAUTH_REDIRECT_URL = '';
+export const OAUTH_AUTH_URL = '';
+export const OAUTH_TOKEN_URL = '';
+export const OAUTH_PROFILE_URL = '';
+export const OAUTH_CLIENT_ID = '';
+export const OAUTH_SECRET = '';
